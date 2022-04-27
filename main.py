ecb4b1aa21199f0afb9915767b39c5a05f4a0bea
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2022-04-27 13:23:06 +0200

    fix hparams

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from glob import glob
 import yaml
 from classifiers import NeuralPCAClassifier, MAPClassifier, CNNClassifier
 import pytorch_lightning as pl
-pl.utilities.seed.seed_everything(42)
+import sys
 
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('(%(levelname)s): %(funcName)s:%(lineno)d %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+#logger.disabled = True
+
+pl.utilities.seed.seed_everything(42)
 
 def main(hparams):
     dicts = {"Neural_PCA": [NeuralPCAClassifier, '.png'], "MAP": [MAPClassifier, '.wav'], "CNN": [CNNClassifier, 'png']}
@@ -11,15 +22,22 @@ def main(hparams):
     for key, classifier in dicts.items():
         classifier, file_format = classifier
         interface = classifier(hparams=hparams[key])
+        logger.debug(f"classifier: {key}")
 
         if hparams[key]["eval"]:
+            logger.debug("evaluating...")
             with open(f"{key}_predictions.txt", 'w') as f:
-                for file in glob(f"{hparams['eval_dataset']}/*{file_format}"):
+                for file in glob(f"{hparams['eval_dir']}/*{file_format}"):
                     soft, hard = interface.predict(file)
                     print(f"{file} {soft:.2f} {hard}", file=f)
+            logger.debug(" DONE")
 
 
 # parse hyperpyyaml
-with open("hyperparams.yaml", 'r') as stream:
+with open(sys.argv[1], 'r') as stream:
     hparams = yaml.safe_load(stream)
+hparams |= hparams["default"]
+del hparams['default']
+# print(hparams.keys())
+# exit(0)
 main(hparams)
